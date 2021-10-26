a9546fb117771eb6a18f8ef3161a68e24f0c30db
rachalabidi <74420254+rachalabidi@users.noreply.github.com>
2021-10-27 00:02:52 +0200

    first commit

---
+++ SCC.js
@@ -0,0 +1,28 @@
+
+var particles = Particles.init({
+    selector: ".background",
+    color: ["#03dac6", "#ff0266", "#000000"],
+    connectParticles: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          color: ["#faebd7", "#03dac6", "#ff0266"],
+          maxParticles: 43,
+          connectParticles: false
+        }
+      }
+    ]
+  });
+  window.onload = function () {
+    Particles.init({
+      selector: ".background"
+    });
+  };
+//   ****************************
+var typing=new Typed(".text" , {
+    strings: [" "," ", "Start ", " Coding ", " Club"," "],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
